15b6dc3a6d79d8aeaa186864c5c13240747ef844
Meadow Green <mgreen161@west-mec.org>
2020-01-10 15:17:54 -0700

    completed all steps

--- tny_timer.js
+++ tny_timer.js
@@ -9,13 +9,29 @@
    Date:   1-9-20
 
 */
+showClock();
+setInterval("showClock()", 1000);
 
 function showClock(){
-    var thisDay = new Date("May 19, 2018 9:31:27");
+    var thisDay = new Date();
     var localDate = thisDay.toLocaleDateString();
     var localTime =  thisDay.toLocaleTimeString();
-    document.getElementById(currentTime).innerHTML = "<span>" + localDate + "</span><span>" + localTime + "</span>"; 
+    document.getElementById("currentTime").innerHTML = "<span>" + localDate + "</span><span>" + localTime + "</span>";
+    var j4Date= nextJuly4(thisDay);
+    j4Date.setHours("21");
+
+    var days=(j4Date-thisDay)/(1000*60*60*24);
+    var hrs=(days- Math.floor(days))*24;
+    var mins=(hrs- Math.floor(hrs))*60;
+    var secs=(mins- Math.floor(mins))*60;
+
+    document.getElementById("dLeft").textContent= Math.floor(days);
+    document.getElementById("hLeft").textContent= Math.floor(hrs);
+    document.getElementById("mLeft").textContent= Math.floor(mins);
+    document.getElementById("sLeft").textContent= Math.floor(secs);
+
 }
+
 
 function nextJuly4(currentDate) {
    var cYear = currentDate.getFullYear();
@@ -24,3 +40,4 @@ function nextJuly4(currentDate) {
    if ((jDate - currentDate) < 0) jDate.setFullYear(cYear + 1);
    return jDate;
 }
+
